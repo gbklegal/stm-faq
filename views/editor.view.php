411abfb6230e5b87a31c74be1a0b2a64816c1221
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-01-11 17:20:50 +0100

    Update editor.view.php

--- views/editor.view.php
+++ views/editor.view.php
@@ -1,8 +1,49 @@
 <?php
 
-$wp_editor_settings = [
-    'media_buttons' => false,
-    'drag_drop_upload' => false
-];
+$itemId = $_GET['item_id'] ?? false;
+$getAction = $_POST['action'] ?? false;
 
-wp_editor('Test', 'textarea', $wp_editor_settings);
+$faqQuestionValue = '';
+$faqAnswerValue = '';
+
+switch ($getAction) {
+    case 'create':
+        $itemQuestion = $_POST['faq_question'];
+        $itemAnswer = $_POST['faq_answer'];
+        createFaqItem($itemQuestion, $itemAnswer);
+        break;
+}
+
+if ($itemId !== false) {
+    $faqItem = getFaqItem($itemId);
+    $faqQuestionValue = $faqItem['question'];
+    $faqAnswerValue = $faqItem['answer'];
+}
+
+?>
+
+<form method="post">
+    <h1 class="wp-heading-inline"><?php echo get_admin_page_title(); ?></h1>
+    <hr>
+    <label class="stm-faq-label">Frage</label>
+    <input name="faq_question" placeholder="Frage hier eingeben" class="stm-faq-input" value="<?php echo $faqAnswerValue; ?>">
+
+    <label class="stm-faq-label">Antwort</label>
+    <?php
+
+    $wpEditorSettings = [
+        'media_buttons' => false,
+        'drag_drop_upload' => false,
+        'textarea_name' => 'faq_answer'
+    ];
+
+    wp_editor($faqAnswerValue, 'textarea', $wpEditorSettings);
+
+    ?>
+
+    <p class="submit">
+        <input type="submit" name="submit" id="submit" class="button-primary" value="<?php echo $submitValue; ?>">
+    </p>
+
+    <input type="hidden" name="action" value="create">
+</form>
