110c80c51dbee5349223a9d8d65c0da9ce99ddeb
Tobias Röder <24211899+tobiasroeder@users.noreply.github.com>
2022-01-11 17:20:41 +0100

    Add getFaqItem and createFaqItem
    createFaqItem function has some bugs to be fixed

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
-    - editFaqItem
-    - createFaqItem
+    - editFaqItem (prepared statement!)
+    - createFaqItem (prepared statement!)
  */
 
 
@@ -13,7 +13,47 @@
 function getFaqItems():array {
     global $wpdb;
 
-    $results = $wpdb->get_results('SELECT question, answer FROM stm_faq ORDER BY position ASC', 'ARRAY_A');
+    $results = $wpdb->get_results('SELECT id, question, answer FROM stm_faq ORDER BY position ASC', 'ARRAY_A');
 
     return $results;
 }
+
+/**
+ * Get a specific FAQ Item by ID that returns all Q and A from database
+ * @param string|int
+ * @return array
+ */
+function getFaqItem( $id ):array {
+    global $wpdb;
+
+    $results = $wpdb->get_results("SELECT id, question, answer FROM stm_faq WHERE id = {$id}", 'ARRAY_A');
+    $result = $results[0];
+
+    if (!$result)
+        return [];
+
+    return $result;
+}
+
+
+
+
+
+/**
+ * 
+ */
+function createFaqItem( string $question, string $answer ):bool {
+    global $wpdb;
+
+    $wpdb->insert('stm_faq', [
+        'question' => $question,
+        'answer' => $answer
+    ]);
+
+    var_dump($wpdb->insert_id);
+
+    return false;
+}
+
+
+// createFaqItem( 'Test Frage 2', 'Test Antwort 2' );
